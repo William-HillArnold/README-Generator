9ea249dfd9b6f263942d582174370d0eb7858d31
williamhillarnold@yahoo.com <97689293+will9105@users.noreply.github.com>
2022-07-28 23:30:09 -0400

    README created and video link

---
+++ README.md
@@ -0,0 +1,34 @@
+
+# README Generator
+
+[![License: MIT](https://img.shields.io/badge/License-MIT-yellow.svg)](https://opensource.org/licenses/MIT)
+
+## Table of Content
+- [Project Description](#description)
+- [Installation](#installation)
+- [Usage](#usage)
+- [Credits](#credits)
+- [License](#license)
+- [Questions](#questions)
+
+
+## Description 
+Application that creates professional README file
+
+## Installation
+Use node.js in terminal to bring up prompts and generate README based on answers to 
+
+## Usage
+questions Use to easily generate professional README file and save time 
+
+## Credits 
+William Hill-Arnold
+
+## License
+undefined
+
+## Questions 
+- Github: https://github.com/William-HillArnold
+- Email: williamhillarnold@yahoo.com
+
+Video link https://app.castify.com/view/1d2716d9-31d8-457a-8cbf-8734269c419d
--- index.js
+++ index.js
@@ -1,65 +1,112 @@
-// TODO: Include packages needed for this application
+//***** Packages needed for this application *****
+
 const inquirer = require("inquirer");
 const fs = require("fs");
-// TODO: Create an array of questions for user input
+renderLicenseBadge('');
+// *****  Array of questions for user input *****
 
 const promptUser = () => {
-    return inquirer.prompt([
+  return inquirer.prompt([
+    {
+      type: "input",
+      name: "title",
+      message: "What is the title of your project?",
+    },
+    {
+      type: "input",
+      name: "description",
+      message: "Please describe your project.",
+    },
+    {
+      type: "input",
+      name: "installation",
+      message: "What are the steps required to install your project?",
+    },
+    {
+      type: "input",
+      name: "usage",
+      message: "Please describe how to use your project.",
+    },
+    {
+      type: "input",
+      name: "credits",
+      message: "Please list your collaborators.",
+    },
+    {
+      type: "list",
+      name: "licence",
+      message: "Please list your license.",
+      choices: ['MIT', 'ISC', 'GNUPLv3']
+    },
+    {
+      type: "input",
+      name: "github",
+      message: "Please enter your github username for questions.",
+    },
+    {
+      type: "input",
+      name: "email",
+      message: "Please enter your email for questions.",
+    },
+  ]);
+};
 
-    { type: 'input',
-    name: 'title',
-    message: 'What is the title of your project?' },
-    { type: 'input',
-    name: 'description',
-    message: 'Please describe your project.' },
-    { type: 'input',
-    name: 'installation',
-    message: 'What are the steps required to install your project?' },
-    { type: 'input',
-    name: 'usage',
-    message: 'Please describe how to use your project.' },
-    { type: 'input',
-    name: 'credits',
-    message: 'Please list your collaborators.' },
-    { type: 'input',
-    name: 'license',
-    message: 'Please list your license.' },
+// ***** function to render README licence badge *****
 
-]);
+function renderLicenseBadge(license) {
+  const badges = {
+    mit: `[![License: MIT](https://img.shields.io/badge/License-MIT-yellow.svg)](https://opensource.org/licenses/MIT)`,
+    isc: `[![License: ISC](https://img.shields.io/badge/License-ISC-blue.svg)](https://opensource.org/licenses/ISC)`,
+    gungplv3: `[![License: GPL v3](https://img.shields.io/badge/License-GPLv3-blue.svg)](https://www.gnu.org/licenses/gpl-3.0)`,
+    }
+  return badges[license];
 }
 
- const gernerateREADME = ({title, description, installation, usage, credits, license}) =>
-    `
-    ${title}
-    -- Description -- 
-    ${description}
-    -- Installation --
-    ${installation}
-    -- Usage --
-    ${usage}
-    -- Credits --
-    ${credits}
-    -- License --
-    ${license}
-    `;
+// ***** function to write README file *****
 
-    const init = () => {
-        promptUser()
-     
-         .then((answers) => fs.writeFileSync('README.md', gernerateREADME(answers)))
-         .then(() => console.log('Successfully created README.md'))
-         .catch((err) => console.log(err));
-     
-        };
-     
-      init();
-     
-/*
-// TODO: Create a function to write README file
-function writeToFile(fileName, data) {
-// TODO: Create a function to initialize app
-function init() {}
+const gernerateREADME = (answers) =>
+  `
+# ${answers.title}
 
-// Function call to initialize app
+[![License: MIT](https://img.shields.io/badge/License-MIT-yellow.svg)](https://opensource.org/licenses/MIT)
+
+## Table of Content
+- [Project Description](#description)
+- [Installation](#installation)
+- [Usage](#usage)
+- [Credits](#credits)
+- [License](#license)
+- [Questions](#questions)
+
+
+## Description 
+${answers.description}
+
+## Installation
+${answers.installation}
+
+## Usage
+${answers.usage}
+
+## Credits 
+${answers.credits}
+
+## License
+${answers.license}
+
+## Questions 
+- Github: ${answers.github}
+- Email: ${answers.email}
+
+Video link https://app.castify.com/view/1d2716d9-31d8-457a-8cbf-8734269c419d
+`;
+//*****  Function call to initialize app *****
+
+const init = () => {
+  promptUser()
+    .then((answers) => fs.writeFileSync("README.md", gernerateREADME(answers)))
+    .then(() => console.log("Successfully created README.md"))
+    .catch((err) => console.log(err));
+};
+
 init();
-*/  
